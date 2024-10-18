fa072cf39fd8f724f15db936d324bffb5800be47
SaurabhKadam02 <saurabhkadam653@gmail.com>
2024-10-18 09:32:37 +0000

    add images

--- src/main.js
+++ src/main.js
@@ -1,24 +1,22 @@
+// DOM Selection
 const selectEl = document.querySelector("select");
-const carpuselContainerEl = document.querySelector("carousel-inner");
-console.log(selectEl, carpuselContainerEl);
+const carouselContainerEl = document.querySelector(".carousel-inner");
 
 const BASE_URL = "https://dog.ceo/api/";
-console.log("started");
 
-// ====get the list of dog breeds from api
+// === MARK: Fetch
+// Gets the list of dog breeds from api
 async function getDogsList() {
   try {
     const response = await fetch(`${BASE_URL}breeds/list/all`);
-
     const data = await response.json();
     return Object.keys(data.message);
   } catch (err) {
     console.log(err);
   }
 }
-getDogsList();
 
-// get list of ten images on breed
+// Gets list of 10 images on breed
 async function getDogImages(breed) {
   try {
     const res = await fetch(`${BASE_URL}breed/${breed}/images`);
@@ -28,23 +26,41 @@ async function getDogImages(breed) {
     console.log(err);
   }
 }
-console.log(getDogsList());
 
-async function renderoptions() {
-  const breedlist = await getDogsList();
-  for (let breed of breedlist) {
-    const Option = document.createElement("option");
-    Option.textContent = breed;
-    selectEl.appendChild(Option);
+// === MARK: Render
+async function renderOptions() {
+  const breedList = await getDogsList();
+
+  for (breed of breedList) {
+    const option = document.createElement("option");
+    option.textContent = breed[0].toUpperCase() + breed.slice(1).toLowerCase();
+    option.value = breed;
+    selectEl.appendChild(option);
   }
 }
-renderoptions();
 
-async function rendecarousel() {
+renderOptions();
+
+async function renderCarousel(breed) {
+  carouselContainerEl.innerHTML = "";
   const images = await getDogImages(breed);
-  console.log(images);
+
+  //   Add active to just the first item
+  for (let i = 0; i < images.length; i++) {
+    const div = document.createElement("div");
+    div.classList.add("carousel-item", i === 0 && "active");
+    div.innerHTML = `<img
+              src="${images[i]}"
+              class="d-block w-100 rounded-3"
+              alt="..."
+            />`;
+    carouselContainerEl.appendChild(div);
+  }
 }
 
+// This runs on select change
 selectEl.addEventListener("change", function (e) {
-  rendecarousel(e.target.value);
+  renderCarousel(e.target.value);
 });
+
+renderCarousel("affenpinscher");
